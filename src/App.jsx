9ab516e76da42debac97497aadb9dc0bd67b1f09
Bohdan_Kartavenko <bogdankartavenko@gmail.com>
2023-02-21 23:37:35 +0200

    add propTypes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import { Notification } from "./components/notification";
 import styles from './App.module.css';
-import { IconCheck } from "./components/notification/icons/IconCheck";
-import { IconInfo } from "./components/notification/icons/IconInfo";
-import { IconError } from "./components/notification/icons/IconError";
 
 export function App() {
   return (
@@ -11,18 +8,15 @@ export function App() {
         <div className={styles.title}>Notifications</div>
         <Notification
           text='Notification'
-          color={styles.warning}
-          icon={<IconInfo />}
+          type="success"
         />
         <Notification
           text='Notification'
-          color={styles.success}
-          icon={<IconCheck />}
+          type="warning"
         />
         <Notification
           text='Notification'
-          color={styles.danger}
-          icon={<IconError />}
+          type="danger"
         />
       </div>
     </>
--- src/components/notification/Notification.jsx
+++ src/components/notification/Notification.jsx
@@ -1,6 +1,21 @@
 import clsx from 'clsx';
 import styles from './Notification.module.css';
+import PropTypes from 'prop-types';
+import IconCheck from "../../assets/icons/Check.svg";
+import IconInfo from "../../assets/icons/Info.svg";
+import IconError from "../../assets/icons/Error.svg";
 
-export const Notification = ({icon, text, color}) => {
-    return <div className={clsx(styles.container, color)}> <div className={styles.icon}>{icon}</div> {text}</div>
+const icons = {
+    success: IconCheck,
+    warning: IconInfo,
+    danger: IconError,
+}
+
+export const Notification = ({ text, type }) => {
+    return <button className={clsx(styles.container, styles[type])}> <img className={styles.icon} src={icons[type]} alt={type} /> {text}</button>
+};
+
+Notification.propTypes = {
+    type: PropTypes.oneOf(['success', 'warning', 'danger']).isRequired,
+    text: PropTypes.string.isRequired,
 };
